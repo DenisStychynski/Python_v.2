6992d5973f479ee6a07d6477a509a743d78652cf
Denis <stychynskidenis@gmail.com>
2023-09-17 12:18:25 +0300

    ДЗ

--- Controller.py
+++ Controller.py
@@ -1,30 +1,30 @@
 
 
-import Function as f
+import Function as d
 import faulthandler
-import Ui
+import Trait
 
 
 def run():
     input_from_user = ''
     while input_from_user != '7':
-        Ui.menu()
+        Trait.menu()
         input_from_user = input().strip()
         if input_from_user == '1':
-            f.show('all')
+            d.show('all')
         if input_from_user == '2':
-            f.add()
+            d.add()
         if input_from_user == '3':
-            f.show('all')
-            f.id_edit_del_show('del')
+            d.show('all')
+            d.id_edit_del_show('del')
         if input_from_user == '4':
-            f.show('all')
-            f.id_edit_del_show('edit')
+            d.show('all')
+            d.id_edit_del_show('edit')
         if input_from_user == '5':
-            f.show('date')
+            d.show('date')
         if input_from_user == '6':
-           f.show('id')
-           f.id_edit_del_show('show')
+           d.show('id')
+           d.id_edit_del_show('show')
         if input_from_user == '7':
-            Ui.goodbuy()
+            Trait.goodbuy()
             break
